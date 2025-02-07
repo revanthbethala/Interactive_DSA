fe76424a10428a6ae2a0c3a7ae52e84ac558aeb1
Revanth <bethalarevanth@gmail.com>
2025-02-07 12:44:48 +0530

    chore: update ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { Hero } from "@/components/landing/hero";
 import { Features } from "@/components/landing/features";
 import { TechStack } from "@/components/landing/tech-stack";
-import { CTA } from "@/components/landing/cta";
 import { Navbar } from "@/components/navigation/navbar";
 
 export default function Home() {
@@ -10,8 +9,6 @@ export default function Home() {
       <Navbar />
       <Hero />
       <Features />
-      <TechStack />
-      <CTA />
     </div>
   );
 }
